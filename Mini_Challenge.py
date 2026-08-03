fd82c40370b4ab389a84ab566ef050b91bad7e44
Madhusmita Behera <madhusmitab0000@gmail.com>
2026-08-03 23:09:25 +0530

    Added basic program practice

---
+++ Mini_Challenge.py
@@ -0,0 +1,12 @@
+# Enter your name: Madhu
+# Enter your age: 21
+
+# Hello Ismail!
+# Welcome to Placement Mission 2026.
+# Your age is 21.
+
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+print( "Hello",name)
+print("Welocome to placement Mission 2026")
+print("Your age is:",age)
---
+++ Question-02.py
@@ -0,0 +1,1 @@
+print ("Madhusmita Behera")
