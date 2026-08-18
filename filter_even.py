602fbc7435a5ce0f3b3f2f29d75843ea69625f9f
Madhusmita Behera <madhusmitab0000@gmail.com>
2026-08-18 22:13:45 +0530

    Added filter even

---
+++ filter_even.py
@@ -0,0 +1,12 @@
+numbers=[1,2,3,4,5,6]
+
+# squared = []
+
+# for i in numbers:
+#     squared.append(i*i)
+
+# squared = [i*i for i in numbers]
+# print(squared)
+
+even=[i for i in numbers if i%2==0]
+print(even)
